f253e9b23f0f27b28f27af85619e268a0810af2a
prabashwara65 <prabashwara65@gmail.com>
2025-02-22 12:12:35 +0530

    commit

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,13 @@
-const express = require('express')
+require('dotenv').config()
+
+const express = require("express");
 
 const app = express();
 
-app.get('/' ,(req , res) => {
-    res.json({mssg: "routes run okay"})
-})
+app.get("/", (req, res) => {
+  res.json({ mssg: "routes run okay" });
+});
 
-app.listen(4000 , ()=> {
-    console.log("Server runs on port 4000")
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server runs on port " + process.env.PORT);
+});
