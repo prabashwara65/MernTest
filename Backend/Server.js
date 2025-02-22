1cb284c583f65a3c0b9b7696d1083cf925f158d3
prabashwara65 <prabashwara65@gmail.com>
2025-02-22 12:51:01 +0530

    routes test perfectly

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 
 const express = require("express");
+const workoutRoutes = require("./routes/workout")
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ mssg: "routes run okay" });
-});
+//middleware
+app.use(express.json())
+
+//routes
+app.use('/api/workouts' , workoutRoutes)
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server runs on port " + process.env.PORT);
