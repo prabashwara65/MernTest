5a261e50d0f5c8f2d8293b0641ae986b2ab1506e
prabashwara65 <prabashwara65@gmail.com>
2025-02-22 14:02:41 +0530

    success test by post using postmon

--- Backend/routes/workout.js
+++ Backend/routes/workout.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const workoutModel = require('../models/workoutModel')
 
 const router = express.Router();
 
 
 router.get('/' , (req , res)=> {
     res.json({mssg: "get all workouts"})
+})
+
+router.post('/' , async (req , res) => {
+    const {title , reps , load} = req.body;
+
+    try{
+        const workout = await workoutModel.create({ title , reps , load})
+        res.status(200).json(workout)
+    }catch(error){
+        res.status(400).json({error : error.message})
+    }
 })
 
 
