20a3ce2953cf70110f45f0a6de630e173c87ce70
prabashwara65 <prabashwara65@gmail.com>
2025-02-26 11:05:12 +0530

    created controllers

--- Backend/controller/workoutController.js
+++ Backend/controller/workoutController.js
@@ -1,13 +1,26 @@
 const workoutModel = require('../models/workoutModel')
 
 
+//get all workout
+const getWorkouts = async (req , res) => {
 
-
-//get all workout 
-
+    const workouts = await workoutModel.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+}
 
 //get single workout 
+const getWorkout = async (req , res) => {
 
+    const { id } = req.body;
+
+    const workout = await workoutModel.findById(id)
+
+    if(!workout){
+        return res.status(404).json({error: error.message})
+    }
+
+    res.status(200).json(workout)
+}
 
 //post new workout
 const createWorkout = async (req , res) => {
@@ -29,4 +42,6 @@ const createWorkout = async (req , res) => {
 
 module.exports = {
     createWorkout,
+    getWorkouts,
+    getWorkout,
 }
