2f7ad6e79bc1b587988ed6e05a105d26f12af404
prabashwara65 <prabashwara65@gmail.com>
2025-03-03 15:48:25 +0530

    crud is working

--- frontend/src/Components/WorkoutForm.jsx
+++ frontend/src/Components/WorkoutForm.jsx
@@ -2,20 +2,40 @@ import { useState } from "react";
 
 const WorkoutForm = () => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault;
-
-        const workout = {title , reps , load}
-
-        const response = await fetch('api/workout', {
-            
-        })
-    }
-
     const [title , setTitle] = useState('');
     const [load , setLoad] = useState('');
     const [reps , setReps] = useState('');
     const [error , setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const workout = { title , reps , load }
+
+        const response = await fetch('http://localhost:4000/api/workouts', {
+            method : "POST",
+            body: JSON.stringify(workout),
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        })
+
+        const json = await response.json;
+
+        if(!response.ok){
+            setError(json.error)
+        }
+
+        if(response.ok){
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+
+            console.log("New record has been added ")
+        }
+    }
+
 
     return (
         <form className="" onSubmit={handleSubmit}>
@@ -32,7 +52,7 @@ const WorkoutForm = () => {
             <label>Excercise Load</label>
             <input 
                 className="border"
-                type="text"
+                type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
             />
@@ -40,13 +60,14 @@ const WorkoutForm = () => {
             <label>Excercise Reps</label>
             <input 
                 className="border"
-                type="text"
+                type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
             />
 
 
             <button>Add New Workout</button>
+            {error && <div className="text-red-500">{error}</div>}
 
         </form>
     )
