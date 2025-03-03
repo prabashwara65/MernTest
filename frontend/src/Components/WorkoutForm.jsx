8be75a978cf2dff5a473fd95b46e95ea87e51d38
prabashwara65 <prabashwara65@gmail.com>
2025-03-03 15:30:08 +0530

    commit

--- frontend/src/Components/WorkoutForm.jsx
+++ frontend/src/Components/WorkoutForm.jsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 
 const WorkoutForm = () => {
 
+    const handleSubmit = async (e) => {
+        e.preventDefault;
+
+        const workout = {title , reps , load}
+
+        const response = await fetch('api/workout', {
+            
+        })
+    }
+
     const [title , setTitle] = useState('');
     const [load , setLoad] = useState('');
     const [reps , setReps] = useState('');
+    const [error , setError] = useState(null);
 
     return (
-        <form className="border-4">
+        <form className="" onSubmit={handleSubmit}>
             <h3>Add new Workout</h3>
 
             <label>Excercise Title</label>
             <input 
+                className="border"
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
@@ -19,6 +31,7 @@ const WorkoutForm = () => {
 
             <label>Excercise Load</label>
             <input 
+                className="border"
                 type="text"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
@@ -26,10 +39,14 @@ const WorkoutForm = () => {
 
             <label>Excercise Reps</label>
             <input 
+                className="border"
                 type="text"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
             />
+
+
+            <button>Add New Workout</button>
 
         </form>
     )
